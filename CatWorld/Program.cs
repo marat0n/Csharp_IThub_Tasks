32365ab2d2d88b8afca1e6df93138cf06a537f24
Marat Gevorkyan <marat2255a@gmail.com>
2022-09-21 18:07:22 +0300

    rename SomeTasks to CatWorld

---
+++ CatWorld/Program.cs
@@ -0,0 +1,9 @@
+﻿using SomeTasks;
+
+Cat cat = new("Chris Hemsworth");
+
+cat.Feed(700);
+Console.WriteLine(cat.WellFedPercent);
+cat.Pee();
+
+Console.WriteLine(cat.Equals(cat.DeepCopy()));
--- SomeTasks/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using SomeTasks;
-
-Cat cat = new("Chris Hemsworth");
-
-cat.Feed(700);
-Console.WriteLine(cat.WellFedPercent);
-cat.Pee();
-
-Console.WriteLine(cat.Equals(cat.DeepCopy()));
